0aa98a19c338796cf8f0ab8f9edb9acb3ae98a5d
Asad Ullah <70887508+asadullahkhaan@users.noreply.github.com>
2021-12-26 01:48:16 +0500

    eye function done

--- lib/components/rounded_password_field.dart
+++ lib/components/rounded_password_field.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:following_the_prophet/components/text_field_container.dart';
 
-class RoundedPasswordField extends StatelessWidget {
+class RoundedPasswordField extends StatefulWidget {
   final ValueChanged<String> onChanged;
   final TextEditingController controller;
   const RoundedPasswordField({
@@ -11,22 +11,45 @@ class RoundedPasswordField extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<RoundedPasswordField> createState() => _RoundedPasswordFieldState();
+}
+
+class _RoundedPasswordFieldState extends State<RoundedPasswordField> {
+ bool _showPassword = true;
+
+  void _togglevisibility() {
+    setState(() {
+      _showPassword = !_showPassword;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return TextFieldContainer(
       child: TextField(
-        obscureText: true,
-        controller: controller,
-        onChanged: onChanged,
+        obscureText: _showPassword,
+        controller: widget.controller,
+        onChanged: widget.onChanged,
         cursorColor: Color(0xFF080603),
         decoration: InputDecoration(
           hintText: "Password",
           icon: Icon(
+            
             Icons.lock,
             color: Color(0xFF080603),
           ),
-          suffixIcon: Icon(
-            Icons.visibility,
-            color: Color(0xFF080603),
+          suffixIcon: GestureDetector(
+            onTap: (){
+              _togglevisibility();
+            },
+            child: _showPassword ? Icon(
+              Icons.visibility_off,
+              color: Color(0xFF080603),
+            )
+            :Icon(
+              Icons.visibility,
+              color: Color(0xFF080603),
+            ),
           ),
           border: InputBorder.none,
         ),
